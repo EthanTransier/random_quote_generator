b968c11fd17427f8698361d52eb7dec936858a9b
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2023-12-08 00:37:22 -0700

    Finished, and it's peak

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+This may be the greatest thing I've ever created.
+Just press the button to get a new quote, if you can't figure that out then honestly what are you even doing.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/ButtonGen.jsx
+++ src/components/ButtonGen.jsx
@@ -2,8 +2,7 @@ import {useState, useEffect} from 'react'
 
 const ButtonGen = () => {
     const [quotes, setQuotes] = useState([])
-    const [heart, setHeart] = useState(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3Z"/></svg>)
-    const [heartChecker, setHeartChecker] = useState(false)
+   
   useEffect(() =>{
     fetch('https://api.quotable.io/random').then(response=>{
         if(response.status >=200 && response.status < 300){
@@ -19,18 +18,6 @@ const ButtonGen = () => {
   return(
     <>
         <h3>"{quotes.content}" - {quotes.author}</h3>
-        <div className='actions'>
-          <button className='heartButton' onClick={()=>{
-            if(heartChecker){
-              setHeartChecker(false)
-              setHeart(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3Z"/></svg>)
-            }else{
-              setHeartChecker(true)
-              setHeart(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#FF3A51" d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z"/></svg>)}}
-            }
-            >{heart}</button>
-          <button className="copyButton"><svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" viewBox="0 0 256 256"><path fill="currentColor" d="M216 32H88a8 8 0 0 0-8 8v40H40a8 8 0 0 0-8 8v128a8 8 0 0 0 8 8h128a8 8 0 0 0 8-8v-40h40a8 8 0 0 0 8-8V40a8 8 0 0 0-8-8Zm-56 176H48V96h112Zm48-48h-32V88a8 8 0 0 0-8-8H96V48h112Z"/></svg></button>
-        </div>
         <button className='newQuote' onClick={()=>{
           window.location.reload()
         }}>New Quote</button>
